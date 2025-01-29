eafcb962fb8ed5be0bbcdf5d7c479acd34f9ea33
Tamir Dingjan <tamir.dingjan@gmail.com>
2025-01-29 13:56:56 +0200

    Add large maze example to main

--- main.py
+++ main.py
@@ -39,8 +39,31 @@ class Window(Tk):
 
 
 if __name__ == "__main__":
-    win = Window(800, 600)
 
-    win.draw_line(Line(Point(800, 600), Point(200, 33)), "orange")
+    win = Window(1200, 800)
+
+    n_cols = 50
+    n_rows = 30
+
+    maze = Maze(
+        x1=50,
+        y1=50,
+        num_rows=n_rows,
+        num_cols=n_cols,
+        cell_size_x=20,
+        cell_size_y=20,
+        win=win,
+        seed=0,
+    )
+
+    maze._draw_all_cells()
+
+    maze._break_entrance_and_exit()
+
+    maze._break_walls_r(0, 0)
+
+    maze._reset_cells_visited()
+
+    maze.solve()
 
     win.run()
